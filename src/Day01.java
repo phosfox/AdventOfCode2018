aff3890c853bb3702c26c98d28a98d4079503069
phosfox <mordredur@googlemail.com>
2018-12-01 15:13:07 +0100

    Solved Day01 second part

--- src/Day01.java
+++ src/Day01.java
@@ -7,6 +7,11 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 
 public class Day01 {
@@ -14,8 +19,39 @@ public class Day01 {
   public int calibrateFrequency(String file) throws IOException {
 
     return Files.lines(Paths.get(file), StandardCharsets.UTF_8)
-    .mapToInt(Integer::parseInt)
+        .mapToInt(Integer::parseInt)
         .sum();
+  }
+
+  public int calibrateFrequencyTwice(String file) throws IOException {
+    ArrayList<Integer> freqChanges = new ArrayList<>();
+    Files.lines(Paths.get(file), StandardCharsets.UTF_8)
+        .mapToInt(Integer::parseInt)
+        .forEach(freqChanges::add);
+
+    HashSet<Integer> freqSet = new HashSet<>();
+    int sum = 0;
+
+    for (int i = 0; i < 200; i++) {
+      for (int f : freqChanges) {
+        sum += f;
+        if (!freqSet.add(sum)) {
+          return sum;
+        }
+      }
+    }
+    return 0;
+//    Arrays.parallelPrefix(summedFreqs, Integer::sum);
+//
+
+//
+//    for (int f : summedFreqs) {
+//      if (!freqSet.add(f)) {
+//        System.out.println(f);
+//      }
+//    }
+//
+//    return 0;
   }
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ import java.io.IOException;
 public class Main {
 
   public static void main(String[] args) throws IOException {
-
+    String file = "src\\Day01Input.txt";
     Day01 d1 = new Day01();
-    int freqResult = d1.calibrateFrequency("src\\Day01Input.txt");
-    System.out.println(freqResult);
+
+    //int freqResult = d1.calibrateFrequency(file);
+    //System.out.println(freqResult);
+
+    int freqTwice = d1.calibrateFrequencyTwice(file);
+    System.out.println(freqTwice);
   }
 }
