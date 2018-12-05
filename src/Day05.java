585b199a5b4617f11d68929934c445bd3e15c275
phosfox <mordredur@googlemail.com>
2018-12-05 20:57:58 +0100

    Solved Day05 part 2

--- src/Day05.java
+++ src/Day05.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Day05 {
 
@@ -9,6 +11,34 @@ public class Day05 {
     input = collapsePolymers(input);
     System.out.println(input);
     System.out.println("Polymer length: " + input.length());
+  }
+
+  public void solveDay05Part2(String fileName) throws IOException{
+    String input = getInput(fileName);
+    String helpInp;
+    ArrayList<Integer> polymerSizes = new ArrayList<>();
+
+    for (int i = 97; i <= 122; i++) {
+      helpInp = removeCharFromString((char)i, input);
+      helpInp = collapsePolymers(helpInp);
+      polymerSizes.add(helpInp.length());
+    }
+
+    int min = polymerSizes.stream()
+        .min(Comparator.comparing(Integer::valueOf))
+        .get();
+
+    System.out.println("Length of the shortest polymer: " +min);
+  }
+
+  private String removeCharFromString(char rmC, String s) {
+    StringBuilder sb = new StringBuilder(s);
+    for (int i = sb.length() - 1; i >= 0; i--) {
+      if (rmC == Character.toLowerCase(sb.charAt(i))) {
+        sb.deleteCharAt(i);
+      }
+    }
+    return sb.toString();
   }
 
   private String getInput(String fileName) throws IOException {
--- src/Main.java
+++ src/Main.java
@@ -20,19 +20,8 @@ public class Main {
     String file = "C:\\Users\\Constantin\\IdeaProjects\\AdventOfCode2018\\src\\Inputs\\Day05Input.txt";
     Day05 d = new Day05();
     long sTime = System.nanoTime();
-    d.solveDay05Part1(file);
+    d.solveDay05Part2(file);
     long eTime = System.nanoTime();
-
-
-
-    //Rectangle r1 = new Rectangle(1, 1, 3, 4, 4);
-    //Rectangle r2 = new Rectangle(2, 3, 1, 4, 4);
-    //Rectangle r3 = new Rectangle(3, 5, 5, 2, 2);
-    //Rectangle r4 = new Rectangle(4, 1, 1, 1, 1);
-    //System.out.println(r1.overlapsWith(r2) + " should true");
-    //System.out.println(r1.overlapsWith(r3) + " should false");
-    //System.out.println(r2.overlapsWith(r3) + " should false");
-    //System.out.println(r4.overlapsWith(r1) + " should false");
 
     System.out.println("Duration: " + (eTime - sTime) / 1000000 + "ms");
 
