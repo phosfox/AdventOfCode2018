71fe0c0beaac5b49aa8a43d928c9474763b6c7ed
phosfox <mordredur@googlemail.com>
2018-12-17 00:40:27 +0100

    day9 done

--- src/Day09.java
+++ src/Day09.java
@@ -1,12 +1,49 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
 public class Day09 {
 
-
-  private void solvePart1(){
-    String input = "";
+  public class CircleDeque<T> extends ArrayDeque<T> {
+    public void rotate(int num) {
+      if (num == 0) {
+        return;
+      }
+      if (num > 0) {
+        for (int i = 0; i < num; i++) {
+          T t = this.removeLast();
+          this.addFirst(t);
+        }
+      } else {
+        for (int i = 0; i < Math.abs(num) - 1; i++) {
+          T t = this.remove();
+          this.addLast(t);
+        }
+      }
+    }
   }
 
-  private void main(){
-    solvePart1();
+  private void solvePart1and2() {
+    String input = "";
+    int players = 463;
+    int end = 71787*100;
+    CircleDeque<Integer> circle = new CircleDeque<>();
+    circle.addFirst(0);
+    long[] scores = new long[players];
+
+    for (int i = 1; i <= end ; i++) {
+      if (i % 23 == 0) {
+        circle.rotate(-7);
+        scores[i % players] += i + circle.pop();
+      } else {
+        circle.rotate(2);
+        circle.addLast(i);
+      }
+    }
+    System.out.println(Arrays.stream(scores).max().getAsLong());
+  }
+
+  private void main() {
+    solvePart1and2();
   }
 
   public static void main(String[] args) {
