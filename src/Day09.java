828337cb57bc6d827aab37c01f69b3dea3cd0f4d
phosfox <mordredur@googlemail.com>
2018-12-16 01:13:26 +0100

    starting day9

---
+++ src/Day09.java
@@ -0,0 +1,16 @@
+public class Day09 {
+
+
+  private void solvePart1(){
+    String input = "";
+  }
+
+  private void main(){
+    solvePart1();
+  }
+
+  public static void main(String[] args) {
+    new Day09().main();
+  }
+
+}
